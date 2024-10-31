1f428ab37bd7926a0465ae1fefbb8189bb0d1955
jesus-imanol <jesusimanolcastillo@gmail.com>
2024-10-30 22:28:53 -0600

    quit date

--- src/persistencia/models/Message.ts
+++ src/persistencia/models/Message.ts
@@ -2,17 +2,15 @@ export class Message {
     private messageId: number;
     private appointmentId: number;
     private content: string;
-    private date: string;
 
     constructor(
         messageId: number,
         appointmentId: number,
         content: string,
-        date: string
+
     ) {
         this.messageId = messageId;
         this.appointmentId = appointmentId;
         this.content = content;
-        this.date = date;
     }
 }
