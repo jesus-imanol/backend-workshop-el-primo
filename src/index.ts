a5527cd9981af999dd806f1124b86ee290e9fb2a
jesus-imanol <jesusimanolcastillo@gmail.com>
2024-10-30 21:01:15 -0600

    configure cors

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,34 @@
-import express from 'express'
+import express from 'express';
 import morgan from 'morgan';
 import signale from 'signale';
-import cors from 'cors'
+import cors from 'cors';
 
 import { userRoutes } from './presentacion/routes/userRoute';
-import {appointmentRoutes} from './presentacion/routes/appointmentRoute';
+import { appointmentRoutes } from './presentacion/routes/appointmentRoute';
 import { messageRoutes } from './presentacion/routes/messageRoutes';
-const app = express()
 
-//Middleware para uso del payload
-app.use(express.json()) 
-//Middleware para tener un log personalizado
-app.use(morgan('dev'))
-//Middleware para el uso de cors
-app.use(cors())
+const app = express();
 
-//Recurso users
-app.use("/api/users",userRoutes);
-app.use("/api/appointment",appointmentRoutes);
-app.use("/api/message",messageRoutes);
-app.listen(3000, ()=> {   
-    signale.success('Server open in port 3000')
-})
+// Middleware para el uso de JSON en el payload
+app.use(express.json());
+
+// Middleware para tener un log personalizado
+app.use(morgan('dev'));
+
+// Configuración de CORS para permitir todos los orígenes
+const corsOptions = {
+    origin: '*', 
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+};
+app.use(cors(corsOptions));
+
+// Rutas de la aplicación
+app.use("/api/users", userRoutes);
+app.use("/api/appointment", appointmentRoutes);
+app.use("/api/message", messageRoutes);
+
+// Inicia el servidor en el puerto 3000
+app.listen(3000, () => {   
+    signale.success('Server open in port 3000');
+});
